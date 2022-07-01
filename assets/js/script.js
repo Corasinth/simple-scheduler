1aa9def1bf51fb22deaf3c2a68e3ed34e856d285
Corasinth <beforeyouknowkindness@gmail.com>
2022-07-01 00:39:37 -0500

    store inputted text in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 //=======================Variables=======================
 var currentDay = moment().format("dddd, MMMM Do")
+var currentHour = parseInt (moment().format("HH"), 10)
 
+console.log (currentHour)
 
 
 //=======================Functions=======================
@@ -8,6 +10,14 @@ var currentDay = moment().format("dddd, MMMM Do")
 //Displays current day/month
 $("#currentDay").text(currentDay)
 
+
+//Saves input form value to local storate
+
+$(".saveBtn").on ("click", function(event) {
+    var timeBlock = $(event.target).parent().siblings().eq(1).children(0).attr("id")
+    var saveText = $(event.target).parent().siblings().eq(1).children(0).val()
+    localStorage.setItem (timeBlock, saveText)
+})
 
 //=======================Event Listener=======================
 
