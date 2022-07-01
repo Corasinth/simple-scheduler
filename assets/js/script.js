25204ed35cfba38e15d8c4223a949fa709396765
Corasinth <beforeyouknowkindness@gmail.com>
2022-07-01 02:46:55 -0500

    link colors to current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //=======================Variables=======================
 var currentDay = moment().format("dddd, MMMM Do")
-var currentHour = parseInt (moment().format("HH"), 10)
+var currentHour = parseInt (moment().add(11, "h").format("HH", 10))
 var inputFields = ["nineInput", "tenInput", "elevenInput", "twelveInput", "oneInput", "twoInput", "threeInput", "fourInput", "fiveInput"]
 
 //=======================Functions=======================
@@ -8,27 +8,29 @@ var inputFields = ["nineInput", "tenInput", "elevenInput", "twelveInput", "oneIn
 //Displays current day/month
 $("#currentDay").text(currentDay)
 
-//Saves input form value to local storage
+//Sets input fields to a stored value
+ $(".form-control").each(function(index) {
+    var text = localStorage.getItem(inputFields[index])
+    $(this).val(text)
+  });
+
+  //Takes in current hour and revises css styles accordingly 
+  $(".form-control").each(function(index) {
+    if (currentHour === index+9) {
+        $(this).addClass("present")
+    } 
+    if (currentHour < index+9 ) {
+        $(this).addClass("future")
+    }
+    if (currentHour > index+9 ) {
+        $(this).addClass("past")
+    }
+  });
+
+//=======================Event Listener=======================
+//Saves input form value to local storage on click
 $(".saveBtn").on ("click", function(event) {
     var timeBlock = $(event.target).parent().siblings().eq(1).children(0).attr("id")
     var saveText = $(event.target).parent().siblings().eq(1).children(0).val()
     localStorage.setItem (timeBlock, saveText)
 })
-
-//Loads stored values into input field
-
-
-//Sets input fields to a value
-
-
-
- $(".form-control").each(function(index) {
-    var text =""
-    text = localStorage.getItem(inputFields[index])
-    $(this).val(text)
-    console.log (text)
-    console.log(this)
-  });
-
-//=======================Event Listener=======================
-
