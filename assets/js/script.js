08950f74f1f47503213298cb18e6af60ec6b5c1d
Corasinth <beforeyouknowkindness@gmail.com>
2022-06-30 22:48:48 -0500

    add display for current day

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//=======================Variables=======================
+var currentDay = moment().format("dddd, MMMM Do")
+
+
+
+//=======================Functions=======================
+
+//Displays current day/month
+$("#currentDay").text(currentDay)
+
+
+//=======================Event Listener=======================
+
